e4eca7b7cc7dd34611daa7868fd4373d182913b2
LiaoMingWei <45159981+LiaoMingWei@users.noreply.github.com>
2018-11-27 08:55:04 +0800

    项目初始架构

---
+++ src/main/java/com/example/demo/News1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class News1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(News1Application.class, args);
+	}
+}
